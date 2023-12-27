f19f836674f26f1afc10377498cb322caa50ab4c
Дмитрий Климанов <abunait@mail.ru>
2023-12-27 11:40:00 +1000

    kakoi-to trash

--- Students.cpp
+++ Students.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 #include <string>
 #include <random>
+#include <algorithm>
 
+// Класс Студент
 class Student {
 public:
     Student(const std::string& name) : name(name), grades({}) {}
@@ -28,17 +30,19 @@ private:
     std::vector<int> grades;
 };
 
+// Класс Учитель
 class Teacher {
 public:
     Teacher(const std::string& name, bool goodMood) : name(name), goodMood(goodMood) {}
 
-    void giveGrade(Student& student) {
+    void giveGrades(Student& student, int numGrades) {
         std::random_device rd;
         std::mt19937 gen(rd());
 
-        int baseGrade = student.isExcellentStudent() ? getExcellentGrade(gen) : getRegularGrade(gen);
-
-        student.addGrade(baseGrade);
+        for (int i = 0; i < numGrades; ++i) {
+            int baseGrade = student.isExcellentStudent() ? getExcellentGrade(gen) : getRegularGrade(gen);
+            student.addGrade(baseGrade);
+        }
     }
 
     const std::string& getName() const {
@@ -59,6 +63,30 @@ private:
     }
 };
 
+// Класс Занятие
+class Lesson {
+public:
+    Lesson(const std::string& teacherName, const std::vector<std::string>& studentNames, int numGradesPerStudent)
+        : teacherName(teacherName), studentNames(studentNames), numGradesPerStudent(numGradesPerStudent) {}
+
+    const std::string& getTeacherName() const {
+        return teacherName;
+    }
+
+    const std::vector<std::string>& getStudentNames() const {
+        return studentNames;
+    }
+
+    int getNumGradesPerStudent() const {
+        return numGradesPerStudent;
+    }
+
+private:
+    std::string teacherName;
+    std::vector<std::string> studentNames;
+    int numGradesPerStudent;
+};
+
 class StudentManager {
 public:
     void addStudent(const std::string& name) {
@@ -69,11 +97,26 @@ public:
         teachers.push_back(Teacher(name, goodMood));
     }
 
-    void addGrade(const std::string& studentName, const std::string& teacherName) {
-        auto studentIt = findStudent(studentName);
-        auto teacherIt = findTeacher(teacherName);
-        if (studentIt != students.end() && teacherIt != teachers.end()) {
-            teacherIt->giveGrade(*studentIt);
+    void addLesson(const std::string& teacherName, const std::vector<std::string>& studentNames, int numGradesPerStudent) {
+        lessons.push_back(Lesson(teacherName, studentNames, numGradesPerStudent));
+    }
+
+    void conductLesson(const std::string& teacherName) {
+        auto lessonIt = std::find_if(lessons.begin(), lessons.end(),
+            [teacherName](const Lesson& lesson) {
+                return lesson.getTeacherName() == teacherName;
+            });
+        if (lessonIt != lessons.end()) {
+            auto teacherIt = findTeacher(teacherName);
+            if (teacherIt != teachers.end()) {
+                int numGradesPerStudent = lessonIt->getNumGradesPerStudent();
+                for (const std::string& studentName : lessonIt->getStudentNames()) {
+                    auto studentIt = findStudent(studentName);
+                    if (studentIt != students.end()) {
+                        teacherIt->giveGrades(*studentIt, numGradesPerStudent);
+                    }
+                }
+            }
         }
     }
 
@@ -104,6 +147,7 @@ public:
 private:
     std::vector<Student> students;
     std::vector<Teacher> teachers;
+    std::vector<Lesson> lessons;
 
     std::vector<Student>::iterator findStudent(const std::string& studentName) {
         return std::find_if(students.begin(), students.end(),
@@ -124,53 +168,30 @@ int main() {
     StudentManager studentManager;
 
     // Добавляем студентов
-    studentManager.addStudent("Alice");
-    studentManager.addStudent("Bob");
-    studentManager.addStudent("Charlie");
-    studentManager.addStudent("David");
-    studentManager.addStudent("Eva");
-    studentManager.addStudent("Frank");
-    studentManager.addStudent("Grace");
-    studentManager.addStudent("Harry");
-    studentManager.addStudent("Ivy");
-    studentManager.addStudent("Jack");
-    studentManager.addStudent("Kelly");
-    studentManager.addStudent("Leo");
-    studentManager.addStudent("Mia");
-    studentManager.addStudent("Nick");
-    studentManager.addStudent("Olivia");
-    studentManager.addStudent("Paul");
-    studentManager.addStudent("Quinn");
-    studentManager.addStudent("Rachel");
-    studentManager.addStudent("Sam");
+    std::vector<std::string> studentNames = {
+        "Alice", "Bob", "Charlie", "David", "Eva",
+        "Frank", "Grace", "Harry", "Ivy", "Jack",
+        "Kelly", "Leo", "Mia", "Nick", "Olivia",
+        "Paul", "Quinn", "Rachel", "Sam"
+    };
+    for (const std::string& name : studentNames) {
+        studentManager.addStudent(name);
+    }
 
     // Добавляем преподавателей
-    studentManager.addTeacher("ProfessorSmith", true);  // Преподаватель в хорошем настроении
-    studentManager.addTeacher("DrJohnson", false);      // Преподаватель в плохом настроении
-    studentManager.addTeacher("MsDavis", true);         // Преподаватель в хорошем настроении
+    studentManager.addTeacher("ProfessorSmith", true);  // В хорошем настроении
+    studentManager.addTeacher("DrJohnson", false);      // В плохом настроении
+    studentManager.addTeacher("MsDavis", false);         // В хорошем настроении
 
-    // Выставляем оценки студентам
-    for (int i = 0; i < 5; ++i) {
-        studentManager.addGrade("Alice", "ProfessorSmith");
-        studentManager.addGrade("Bob", "DrJohnson");
-        studentManager.addGrade("Charlie", "ProfessorSmith");
-        studentManager.addGrade("David", "MsDavis");
-        studentManager.addGrade("Eva", "ProfessorSmith");
-        studentManager.addGrade("Frank", "DrJohnson");
-        studentManager.addGrade("Grace", "ProfessorSmith");
-        studentManager.addGrade("Harry", "MsDavis");
-        studentManager.addGrade("Ivy", "DrJohnson");
-        studentManager.addGrade("Jack", "ProfessorSmith");
-        studentManager.addGrade("Kelly", "MsDavis");
-        studentManager.addGrade("Leo", "DrJohnson");
-        studentManager.addGrade("Mia", "ProfessorSmith");
-        studentManager.addGrade("Nick", "MsDavis");
-        studentManager.addGrade("Olivia", "ProfessorSmith");
-        studentManager.addGrade("Paul", "DrJohnson");
-        studentManager.addGrade("Quinn", "MsDavis");
-        studentManager.addGrade("Rachel", "ProfessorSmith");
-        studentManager.addGrade("Sam", "DrJohnson");
-    }
+    // Добавляем занятия
+    studentManager.addLesson("ProfessorSmith", studentNames, 3);  // 3 оценки на занятии
+    studentManager.addLesson("DrJohnson", studentNames, 2);      // 2 оценки на занятии
+    studentManager.addLesson("MsDavis", studentNames, 4);         // 4 оценки на занятии
+
+    // Проводим занятия
+    studentManager.conductLesson("ProfessorSmith");
+    studentManager.conductLesson("DrJohnson");
+    studentManager.conductLesson("MsDavis");
 
     // Выводим оценки студентов
     studentManager.printStudentGrades();
